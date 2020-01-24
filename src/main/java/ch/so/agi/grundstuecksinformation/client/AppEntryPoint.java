ea9dcb60b2ba5861a6577526a04a69a7d49890ef
Stefan Ziegler <edi.gonzales@gmail.com>
2020-01-24 22:24:26 +0100

    use geodienste.ch background... start with map click handler

--- src/main/java/ch/so/agi/grundstuecksinformation/client/AppEntryPoint.java
+++ src/main/java/ch/so/agi/grundstuecksinformation/client/AppEntryPoint.java
@@ -4,22 +4,41 @@ import org.jboss.gwt.elemento.core.Elements;
 import static org.jboss.gwt.elemento.core.Elements.b;
 import static org.jboss.gwt.elemento.core.Elements.div;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.dominokit.domino.ui.loaders.Loader;
 import org.dominokit.domino.ui.loaders.LoaderEffect;
 
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.core.client.JavaScriptObject;
+import com.google.gwt.core.client.JsonUtils;
 import com.google.gwt.i18n.client.NumberFormat;
+import com.google.gwt.json.client.JSONArray;
+import com.google.gwt.json.client.JSONObject;
+import com.google.gwt.json.client.JSONString;
+import com.google.gwt.json.client.JSONValue;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 
 import ch.so.agi.grundstuecksinformation.shared.SettingsResponse;
 import ch.so.agi.grundstuecksinformation.shared.SettingsService;
 import ch.so.agi.grundstuecksinformation.shared.SettingsServiceAsync;
-
+import elemental2.core.Global;
 import elemental2.dom.CSSProperties;
 import elemental2.dom.HTMLDivElement;
 import elemental2.dom.HTMLElement;
+import ol.Coordinate;
+import ol.Feature;
 import ol.Map;
+import ol.MapBrowserEvent;
+import ol.event.EventListener;
+import ol.format.GeoJson;
+
+import static elemental2.dom.DomGlobal.fetch;
+import static elemental2.dom.DomGlobal.console;
+import elemental2.dom.Response;
 
 public class AppEntryPoint implements EntryPoint {
     private MainMessages messages = GWT.create(MainMessages.class);
@@ -53,14 +72,67 @@ public class AppEntryPoint implements EntryPoint {
         el.setAttribute("id", "map");
         Elements.body().add(el);
         
-        Map map = MapPresets.getBlackAndWhiteMap(el.getAttribute("id"));
-        //Loader loader = Loader.create(el, LoaderEffect.PULSE).start();
-        
-        
-        
-    }
+        //Map map = MapPresets.getBlackAndWhiteMap(el.getAttribute("id"));
+        Map map = MapPresets.getCadastralSurveyingWms(el.getAttribute("id"));
+        map.addSingleClickListener(new MapSingleClickListener());
 
-   private static native void updateURLWithoutReloading(String newUrl) /*-{
+        //Loader loader = Loader.create(el, LoaderEffect.PULSE).start();
+    }
+    
+    public final class MapSingleClickListener implements EventListener<MapBrowserEvent> {
+        @Override
+        public void onEvent(MapBrowserEvent event) {
+            Coordinate coordinate = event.getCoordinate();
+
+            //https://api3.geo.admin.ch/rest/services/all/MapServer/identify?geometry=2607381.2857129965,1228422.772096185&geometryFormat=geojson&geometryType=esriGeometryPoint&imageDisplay=1780,772,96&lang=de&layers=all:ch.kantone.cadastralwebmap-farbe&limit=10&mapExtent=2607182.461501755,1228369.7490333454,2607507.9384982456,1228510.9109666548&returnGeometry=true&sr=2056&tolerance=10
+            //String requestUrl = "https://api3.geo.admin.ch/rest/services/all/MapServer/identify?geometry=2607381.2857129965,1228422.772096185&geometryFormat=geojson&geometryType=esriGeometryPoint&imageDisplay=1780,772,96&lang=de&layers=all:ch.kantone.cadastralwebmap-farbe&limit=10&mapExtent=2607182.461501755,1228369.7490333454,2607507.9384982456,1228510.9109666548&returnGeometry=true&sr=2056&tolerance=10";
+            String requestUrl = "https://api3.geo.admin.ch/rest/services/all/MapServer/identify?geometry=2607389.224071799,1228423.0556600185&geometryFormat=geojson&geometryType=esriGeometryPoint&imageDisplay=1780,772,96&lang=de&layers=all:ch.kantone.cadastralwebmap-farbe&limit=10&mapExtent=2607182.461501755,1228369.7490333454,2607507.9384982456,1228510.9109666548&returnGeometry=true&sr=2056&tolerance=10";
+
+            fetch(requestUrl)
+            .then(Response::json)
+            .then(data -> {
+                // Create a valid GeoJSON from returned JSON. 
+                // Later on we can create an ol3 vector layer with the GeoJSON features.
+                JSONObject newFeatureCollection = new JSONObject();
+                newFeatureCollection.put("type", new JSONString("FeatureCollection"));
+                JSONArray newFeaturesArray = new JSONArray();
+
+                JSONObject obj = new JSONObject(JsonUtils.safeEval(Global.JSON.stringify(data)));
+                JSONArray array = obj.get("results").isArray();
+                for (int i=0; i<array.size(); i++) {
+                    
+                    JSONObject feature = array.get(i).isObject();
+                    JSONObject geometry = feature.get("geometry").isObject();
+                    JSONObject properties = feature.get("properties").isObject();
+                    
+                    JSONObject newFeature = new JSONObject();
+                    newFeature.put("type",  new JSONString("Feature"));
+                    newFeature.put("properties", properties);
+                    newFeature.put("geometry", geometry);
+                    
+                    newFeaturesArray.set(i, newFeature);
+                }
+                newFeatureCollection.put("features", newFeaturesArray);
+                
+                Feature[] features = (new GeoJson()).readFeatures(newFeatureCollection.toString());
+                
+                for (Feature feature : features) {
+                    console.log(feature.getProperties().get("label"));
+                    console.log(feature.getGeometry().getExtent().getLowerLeftX());
+                }
+                return null;
+            }).
+            catch_(error -> {
+                console.log(error);
+                return null;
+            });
+
+            
+        }
+    }
+    
+
+    private static native void updateURLWithoutReloading(String newUrl) /*-{
         $wnd.history.pushState(newUrl, "", newUrl);
     }-*/;
 }
--- src/main/java/ch/so/agi/grundstuecksinformation/client/MapPresets.java
+++ src/main/java/ch/so/agi/grundstuecksinformation/client/MapPresets.java
@@ -15,6 +15,9 @@ import ol.layer.LayerOptions;
 import ol.layer.Tile;
 import ol.proj.Projection;
 import ol.proj.ProjectionOptions;
+import ol.source.ImageWms;
+import ol.source.ImageWmsOptions;
+import ol.source.ImageWmsParams;
 import ol.source.Wmts;
 import ol.source.WmtsOptions;
 import ol.tilegrid.TileGrid;
@@ -28,6 +31,88 @@ public class MapPresets {
         throw new AssertionError();
     }
 
+    public static Map getCadastralSurveyingWms(String mapId) {
+        Proj4.defs("EPSG:2056", "+proj=somerc +lat_0=46.95240555555556 +lon_0=7.439583333333333 +k_0=1 +x_0=2600000 +y_0=1200000 +ellps=bessel +towgs84=674.374,15.056,405.346,0,0,0,0 +units=m +no_defs");
+
+        ProjectionOptions projectionOptions = OLFactory.createOptions();
+        projectionOptions.setCode("EPSG:2056");
+        projectionOptions.setUnits("m");
+        projectionOptions.setExtent(new Extent(2420000, 1030000, 2900000, 1350000));
+        
+        Projection projection = new Projection(projectionOptions);
+
+        
+        ImageWmsParams imageWMSParams = OLFactory.createOptions();
+        imageWMSParams.setLayers("Fixpunkte,Hoheitsgrenzen,Nomenklatur,Rohrleitungen,Gebaeudeadressen,Liegenschaften,SOOBJ,SOPT,SOLI,SOSF,LCOBJ,LCSFPROJ,LCSF");
+        imageWMSParams.set("FORMAT", "image/jpeg");
+        imageWMSParams.set("TRANSPARENT", "false");
+        
+        ImageWmsOptions imageWMSOptions = OLFactory.createOptions();
+        //imageWMSOptions.setUrl("http://wms.geo.admin.ch/");
+        imageWMSOptions.setUrl("https://wfs.geodienste.ch/av/deu");
+        imageWMSOptions.setParams(imageWMSParams);
+        imageWMSOptions.setRatio(1.5f);
+
+        ImageWms imageWMSSource = new ImageWms(imageWMSOptions);
+
+        LayerOptions layerOptions = OLFactory.createOptions();
+        layerOptions.setSource(imageWMSSource);
+
+        ol.layer.Image wmsLayer = new ol.layer.Image(layerOptions);
+
+//        // create a projection
+//        ProjectionOptions projectionOptions = OLFactory.createOptions();
+//        projectionOptions.setCode("EPSG:21781");
+//        projectionOptions.setUnits("m");
+
+        
+        /*
+        WmtsOptions wmtsOptions = OLFactory.createOptions();
+        wmtsOptions.setUrl("https://wmts10.geo.admin.ch/1.0.0/ch.kantone.cadastralwebmap-farbe/default/current/2056/{TileMatrix}/{TileCol}/{TileRow}.png");
+        wmtsOptions.setLayer("ch.kantone.cadastralwebmap-farbe");
+        wmtsOptions.setRequestEncoding("REST");
+        wmtsOptions.setFormat("image/png");
+        wmtsOptions.setMatrixSet("EPSG:2056");
+        //wmtsOptions.setStyle("default");
+        wmtsOptions.setProjection(projection);
+        wmtsOptions.setWrapX(true);
+        wmtsOptions.setTileGrid(createWmtsTileGrid(projection));
+
+        Wmts wmtsSource = new Wmts(wmtsOptions);
+
+        LayerOptions wmtsLayerOptions = OLFactory.createOptions();
+        wmtsLayerOptions.setSource(wmtsSource);
+
+        Tile wmtsLayer = new Tile(wmtsLayerOptions);
+        wmtsLayer.setOpacity(1.0);
+        */
+
+        ViewOptions viewOptions = OLFactory.createOptions();
+        viewOptions.setProjection(projection);
+        viewOptions.setResolutions(new double[] { 4000.0, 2000.0, 1000.0, 500.0, 250.0, 100.0, 50.0, 20.0, 10.0, 5.0, 2.5, 1.0, 0.5, 0.25, 0.1 });
+        View view = new View(viewOptions);
+        Coordinate centerCoordinate = new Coordinate(2616491, 1240287);
+
+        view.setCenter(centerCoordinate);
+        view.setZoom(6);
+
+        MapOptions mapOptions = OLFactory.createOptions();
+        mapOptions.setTarget(mapId);
+        mapOptions.setView(view);
+        mapOptions.setControls(new Collection<Control>());
+
+        DefaultInteractionsOptions interactionOptions = new ol.interaction.DefaultInteractionsOptions();
+        interactionOptions.setPinchRotate(false);
+        mapOptions.setInteractions(Interaction.defaults(interactionOptions));
+
+        Map map = new Map(mapOptions);
+        map.addLayer(wmsLayer);
+        
+        return map;
+    }
+    
+    
+    
     public static Map getBlackAndWhiteMap(String mapId) {
         Proj4.defs("EPSG:2056", "+proj=somerc +lat_0=46.95240555555556 +lon_0=7.439583333333333 +k_0=1 +x_0=2600000 +y_0=1200000 +ellps=bessel +towgs84=674.374,15.056,405.346,0,0,0,0 +units=m +no_defs");
 
